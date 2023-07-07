1ee111188740ae599d4632b3ee0b9669356fe1ff
Ya_VaFel <128213334+azikev@users.noreply.github.com>
2023-07-07 20:48:09 +0500

    maded good inventory(but now don't work)

--- Assets/_Client/Scripts/Inventory/Inventory Interface.cs
+++ Assets/_Client/Scripts/Inventory/Inventory Interface.cs
@@ -1,25 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class InventoryInterface : MonoBehaviour
 {
     [SerializeField] private Button _closeInventoryButton;
-    [SerializeField] private RectTransform _inventory;
+    [SerializeField] private Transform _inventory;
+    [SerializeField] private GameObject itemPrefab;
+    private List<InventoryItemSO> _inInventoryItems;
+    public List<InventoryItemSO> _startItems;
 
     private void Start()
     {
+        _inInventoryItems.Clear();
         _closeInventoryButton.onClick.AddListener(InventoryManager.Instance.CloseInventory);
+        AddToInventory();
     }
 
     private void AddToInventory()
     {
-        //for (int i = 0; i < _inventoryManager._inventoryItems.Count; i++)
-        //{
-        //    var item = _inventoryManager._inventoryItems[i];
-        //    var icon = new GameObject(item.name);
-        //    icon.transform.SetParent(_inventory);
-        //    icon.AddComponent<Image>().sprite = item.GetSprite();
-        //}
+        _inInventoryItems = InventoryManager.Instance.GetList();
+        for (int i = 0; i < _inInventoryItems.Count; i++)
+        {
+            var item = _inInventoryItems[i];
+            GameObject Item = Instantiate(itemPrefab, _inventory);
+            Item.transform.SetParent(_inventory);
+            Item.transform.localScale = new Vector3(1, 1, 1);
+
+            var itemText = Item.transform.Find("itemText").GetComponent<Text>();
+            var itemImage = Item.transform.Find("itemImage").GetComponent<Image>();
+
+            itemText.text = item.name;
+            itemImage.sprite = item.GetSprite();
+        }
     }
 }
--- Assets/_Client/Scripts/Managers/Inventory Manager.cs
+++ Assets/_Client/Scripts/Managers/Inventory Manager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class InventoryManager : PersistentSingleton<InventoryManager>
 {
-    [SerializeField] private List<InventoryItemSO> _inventoryItems;
+    private List<InventoryItemSO> _inventoryItems;
     //public event 
 
     public void Initialize()
     {
         _inventoryItems = new List<InventoryItemSO>();
     }
+
+    public List<InventoryItemSO> GetList() => _inventoryItems;
 
     public void AddItem(InventoryItemSO Item)
     {
@@ -19,7 +21,7 @@ public class InventoryManager : PersistentSingleton<InventoryManager>
 
     public void RemoveItem(InventoryItemSO Item)
     {
-        _inventoryItems.Add(Item);
+        _inventoryItems.Remove(Item);
     }
 
     public void CloseInventory()
