1f4f0f2c774e7a941249713e65041220aa9bc4ac
MegaDaveMustain666Deth <129941509+MegaDaveMustain666Deth@users.noreply.github.com>
2023-07-28 16:52:01 +0500

    some work with transitions

--- Assets/_Client/Scripts/Item System/Transition.cs
+++ Assets/_Client/Scripts/Item System/Transition.cs
@@ -5,24 +5,21 @@ public class Transition : Zone
 {
     [SerializeField] private Transform _exitPosition;
     [SerializeField] private Transform _cameraExitPosition;
+    [SerializeField] private float _activateDistances;
 
     protected override void Interact()
     {
         base.Interact();
         StartCoroutine(TransitionToScene());
     }
+
+    public bool DistanceToPlayer => Vector2.Distance(transform.position, Player.Instance.transform.position) <= _activateDistances;
+
     private IEnumerator TransitionToScene()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(Player.Instance.Movement.IsMoving);
 
-        while (Player.Instance.Movement.Agent.isStopped)
-        {
-            yield return new WaitForSeconds(1);
-        }
-
-        Player.Instance.Movement.Agent.enabled = false;
-        Player.Instance.gameObject.transform.position = _exitPosition.position;
-        Player.Instance.Movement.Agent.enabled = true;
+        Player.Instance.Movement.TeleportToPoint(_exitPosition.position);
         Camera.main.transform.position = _cameraExitPosition.position;
     }
 }
--- Assets/_Client/Scripts/Player Motor.cs
+++ Assets/_Client/Scripts/Player Motor.cs
@@ -3,7 +3,8 @@ using UnityEngine.AI;
 
 public class PlayerMotor : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent _agent;   
+    [SerializeField] private NavMeshAgent _agent;
+    private Vector2 _tagetPosition;
 
     public NavMeshAgent Agent => _agent;
 
@@ -16,6 +17,27 @@ public class PlayerMotor : MonoBehaviour
 
     public void Move(Vector2 position)
     {
-        _agent.SetDestination(position);
+        _tagetPosition = position;
+        _agent.isStopped = false;
+        _agent.SetDestination(_tagetPosition);
     }
+
+    public void TeleportToPoint(Vector2 position)
+    {
+        _agent.enabled = false;
+        transform.position = position;
+        _agent.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (St)
+        {
+            _agent.isStopped = true;
+        }
+    }
+
+    public bool St => Vector2.Distance(transform.position, _tagetPosition) <= _agent.stoppingDistance;
+
+    public bool IsMoving() => _agent.isStopped;
 }
