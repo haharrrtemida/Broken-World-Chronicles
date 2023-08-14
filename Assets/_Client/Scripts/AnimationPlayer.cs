69c4d87ba6fc6a57b72e529a5c2448902f432b5c
MegaDaveMustain666Deth <129941509+MegaDaveMustain666Deth@users.noreply.github.com>
2023-08-15 00:38:56 +0300

    maded good animations (but dont work now)pt.2

--- Assets/_Client/Scripts/AnimationPlayer.cs
+++ Assets/_Client/Scripts/AnimationPlayer.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationPlayer
+public class AnimationPlayer : MonoBehaviour
 {
     
 }
--- Assets/_Client/Scripts/Player Motor.cs
+++ Assets/_Client/Scripts/Player Motor.cs
@@ -8,10 +8,8 @@ public class PlayerMotor : MonoBehaviour
     [SerializeField] private Animator _playerAnimator;
 
     public event Action OnReachDestination;
-    public float _angel;
 
     private Vector2 _targetPosition;
-    private bool isTurned;
 
     public void Initialize()
     {
@@ -21,7 +19,8 @@ public class PlayerMotor : MonoBehaviour
     private void StopOnReachDestination()
     {
         _agent.isStopped = true;
-        isTurned = false;
+        _playerAnimator.SetFloat("AnimMoveX", 0);
+        _playerAnimator.SetFloat("AnimMoveY", 0);
     }
 
     public void Move(Vector2 position)
@@ -29,17 +28,9 @@ public class PlayerMotor : MonoBehaviour
         _targetPosition = position;
         _agent.isStopped = false;
         _agent.SetDestination(_targetPosition);
-       _angel = Vector2.Angle(_targetPosition, transform.position);
-       if ((_angel > 90 || _angel > -90) && !isTurned)
-        {
-            _playerAnimator.gameObject.transform.localScale *= new Vector2(-1, 1);
-            isTurned = false;
-        }
-        else if (!isTurned)
-        {
-            _playerAnimator.gameObject.transform.localScale *= new Vector2(-1, 1);
-            isTurned = true;   
-        }
+        position = new Vector2(position.x, position.y).normalized;
+        _playerAnimator.SetFloat("AnimMoveX", position.x);
+        _playerAnimator.SetFloat("AnimMoveY", position.y);
     }
 
     public void TeleportToPoint(Vector2 position)
